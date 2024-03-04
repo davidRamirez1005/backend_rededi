417e1726a6480ea200deda8a017bc6efb70d25ff
davidRamirez1005 <ramirezjd1005@gmail.com>
2024-03-03 22:04:42 -0500

    test: :white_check_mark: se crea enpoint para probar funcionalidad

--- api/controllers/user/domain/userController.js
+++ api/controllers/user/domain/userController.js
@@ -1,7 +1,11 @@
 // import {con} from '../../config/app/atlas.js'
 
 const info = async(req, res) =>{
-    res.send("hola");
+    res.send({
+        name : "User",
+        description : "This is the user domain",
+        version : "1.0.0"
+    });
 }
 
 export { info }
